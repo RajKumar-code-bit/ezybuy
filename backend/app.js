f0bf8215bd2531609f082526c8d0515621ba12ad
RajKumar-code-bit <rajkumarofficial1997@gmail.com>
2024-12-05 22:53:21 +0530

    i added cors function

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,37 @@
 const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const path = require('path')
-dotenv.config({path: path.join(__dirname,'config','config.env')})
-const products = require('./routes/product')
-const orders = require('./routes/order')
-const connectDatabase = require('./config/connectDatabase')
-const cors = require('cors')
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config({ path: path.join(__dirname, "config", "config.env") });
+const products = require("./routes/product");
+const orders = require("./routes/order");
+const connectDatabase = require("./config/connectDatabase");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
 
 //connect database
-connectDatabase()
+connectDatabase();
 
 // to convert json data
-app.use(express.json())
-//for access 
-app.use(cors())
+app.use(express.json());
+//for access
+app.use(cors());
 
-app.use('/api/v1',products)
-app.use('/api/v1',orders)
+app.use(
+  cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+mongoose.connect("mongodb+srv://Rajkumar:Rajkumar@123@cluster0.pp199.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-app.get('/',(req,res)=>{
-res.send("hi")
-})
+app.use("/api/v1", products);
+app.use("/api/v1", orders);
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
 
 //listeing the port
 app.listen(process.env.PORT, () => {
